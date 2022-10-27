73909b14cbc44d93a0d366100641297ff8594a01
CurstinJR <cjrose13@protonmail.com>
2022-10-28 01:50:00 +0200

    Remove images, profile icon and add logout btn
    Reviewed-by: @CurstinJR

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from "@angular/core";
-import {faCircleUser} from "@fortawesome/free-solid-svg-icons";
-import {AppIcon} from "../../_models/app-icon.model";
 import {AuthenticationService} from "../../_services/authentication.service";
 
 @Component({
@@ -9,11 +7,6 @@ import {AuthenticationService} from "../../_services/authentication.service";
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  profile: AppIcon = {
-    icon: faCircleUser,
-    size: "3x"
-  }
 
   constructor(private authenticationService: AuthenticationService) {
   }
